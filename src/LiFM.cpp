ac78ef7aa9d2f6c57da02e3e4875f80b4b8295fc
EAL <elias@takefive.dk>
2025-08-17 14:04:45 +0200

    can now simulate clubs as well

--- src/LiFM.cpp
+++ src/LiFM.cpp
@@ -5,16 +5,28 @@
 #include "LiFM/LiFM.hpp"
 
 #include <iostream>
-#include <ostream>
 
+#include "Dataholder.hpp"
+#include "LiFM-Datatypes/League.hpp"
+#include "LiFM-Datatypes/Match.hpp"
 #include "Utility/ConfigController.h"
 
 LiFM::LiFM() : timeline_(std::make_shared<LiFMTimeline>(std::chrono::system_clock::now()))
 {
-  for (int i = 0; i < 10; i++)
-  {
-    std::cout << timeline_->getDateString() << "\n";
-    std::cout << timeline_->getClockString() << "\n";
-    timeline_->advance();
-  }
+  Dataholder::matches_.push_back(
+    std::make_shared<Match>(0, 1, std::chrono::system_clock::now(), timeline_));
+
+  auto league = League();
+  league.matches_.push_back(0);
+  league.clubs_.push_back(0);
+  league.clubs_.push_back(1);
+
+  Dataholder::clubs_.push_back(std::make_shared<Club>(timeline_));
+  Dataholder::clubs_[0]->name_ = "Brøndby IF";
+  Dataholder::clubs_.push_back(std::make_shared<Club>(timeline_));
+  Dataholder::clubs_[1]->name_ = "FC København";
+  timeline_->advance();
+
+  auto result = league.generateLeagueTable();
+  std::cout << result->generateTableString() << "\n";
 }
